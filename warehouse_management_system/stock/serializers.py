3200b13f62d7d4e027a3fe40997f9e287c8382ee
nigmucode@gmail.com <Programmer@example.com>
2025-06-19 20:42:41 +0530

    some apps removed

--- warehouse_management_system/stock/serializers.py
+++ warehouse_management_system/stock/serializers.py
@@ -5,3 +5,12 @@ class StackSerializer(serializers.ModelSerializer):
     class Meta:
         model = Stack
         fields = '__all__'
+
+    # This handles validation to ensure that the block belongs to the selected warehouse
+    def validate(self, data):
+        block = data.get('block')
+        warehouse = data.get('warehouse')
+        if block.warehouse != warehouse:
+            raise serializers.ValidationError(f"The selected block ({block.block_name}) does not belong to the selected warehouse ({warehouse.warehouse_name}).")
+
+        return data
--- warehouse_management_system/warehouse_management_system/settings.py
+++ warehouse_management_system/warehouse_management_system/settings.py
@@ -38,10 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'common',
     'stock',
     'tile',
-    'users',
     'warehouse',
 ]
 
